b58cd94e8fc812457936cbd68d9d768e8edc59ec
JOHSKI\johan <johannes.skinlo@gmail.com>
2025-02-25 10:37:35 +0100

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   src/main/java/edu/ntnu/idatx2003/capitalize/CapitalizeSelectionTextCommand.java

--- src/main/java/edu/ntnu/idatx2003/capitalize/CapitalizeSelectionTextCommand.java
+++ src/main/java/edu/ntnu/idatx2003/capitalize/CapitalizeSelectionTextCommand.java
@@ -10,7 +10,7 @@ public class CapitalizeSelectionTextCommand extends CapitalizeTextCommand {
 
     @Override
     public String execute(String text) {
-        return text.replace(selection, selection.toUpperCase());
+        return text.replace(selection, selection.substring(0, 1).toUpperCase() + selection.substring(1));
     }
 
     public String getSelection() {
