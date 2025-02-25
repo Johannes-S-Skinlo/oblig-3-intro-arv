3dd35249f7d23758230cd7f18733a3da91ad275c
JOHSKI\johan <johannes.skinlo@gmail.com>
2025-02-25 11:39:02 +0100

    Changes to be committed: 	modified:   src/main/java/edu/ntnu/idatx2003/wrap/WrapLinesTextCommand.java

--- src/main/java/edu/ntnu/idatx2003/wrap/WrapLinesTextCommand.java
+++ src/main/java/edu/ntnu/idatx2003/wrap/WrapLinesTextCommand.java
@@ -34,9 +34,7 @@ public class WrapLinesTextCommand extends WrapTextCommand{
             }
         else {
             for (int i = 0; i < text.length(); i++) {
-                if (!text.substring(i + 1).isEmpty()) {
-                    text = text.substring(0, i) + end + "\n" + opening + text.substring(i + 1);
-                }
+                text = text.substring(0, i) + end + "\n" + opening + text.substring(i + 1);
             }
             return text;
         }
